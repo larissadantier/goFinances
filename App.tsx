2b8aff8af36b09eb7080a988d872d387c1f1da70
Larissa Dantier <larissa_dantier@hotmail.com>
2022-07-05 11:02:40 -0300

    NOVO: adicionando fonte Poppins pela biblioteca do expo

--- App.tsx
+++ App.tsx
@@ -1,11 +1,30 @@
 import React from 'react';
 import {ThemeProvider} from 'styled-components';
+import { 
+  useFonts, 
+  Poppins_400Regular, 
+  Poppins_500Medium, 
+  Poppins_700Bold 
+} from '@expo-google-fonts/poppins';
+import * as SplashScreen from 'expo-splash-screen';
 import theme from './src/global/styles/theme'
 
 import Dashboard from './src/screens/Dashboard/Dashboard';
 
 
 export default function App(): JSX.Element {
+  SplashScreen.preventAutoHideAsync();
+  let [fontsLoaded] = useFonts({
+    Poppins_400Regular, 
+    Poppins_500Medium, 
+    Poppins_700Bold
+  });
+
+  if (!fontsLoaded) return null;
+  
+
+  SplashScreen.hideAsync();
+
   return (
     <ThemeProvider theme={theme}>
       <Dashboard />
